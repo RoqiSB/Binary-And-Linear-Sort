e7363450ebaf08dbfc8d21a1071777a16f9d168b
Roqi SB <roqisb9@gmail.com>
2022-10-24 16:27:31 +0700

    menerima element array

--- Binary-And-Linear-Sort/Program.cs
+++ Binary-And-Linear-Sort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace  SeacrhAlgorthm
 {
@@ -6,9 +7,34 @@ namespace  SeacrhAlgorthm
     {
         //Array to be searched
         int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
         //Get the number of elements to store in the arrray
         int i;
 
-
+        public void input ()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                        break;
+                else
+                    Console.Write("\nArray should have minimum 1 and maximum 20 elements.\n");
+            }
+            //Accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Enter array elements ");
+            Console.WriteLine("----------------------");
+            for (i = 0; i < n; i++);
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
