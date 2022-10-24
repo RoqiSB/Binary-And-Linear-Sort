5aeb7f98ca279c6b652b2edf98a424aa352b2700
Roqi SB <roqisb9@gmail.com>
2022-10-24 16:08:50 +0700

    menambahkan int array

--- Binary-And-Linear-Sort/Program.cs
+++ Binary-And-Linear-Sort/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace  SeacrhAlgorthm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Get the number of elements to store in the arrray
+        int i;
+
+
+    }
+}
