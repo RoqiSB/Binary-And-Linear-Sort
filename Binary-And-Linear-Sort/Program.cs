edabc22d74d300d154e059a63b13a8fa5fdd2e37
Roqi SB <roqisb9@gmail.com>
2022-10-24 17:05:14 +0700

    menambahkan looping pada LinearSearch

--- Binary-And-Linear-Sort/Program.cs
+++ Binary-And-Linear-Sort/Program.cs
@@ -74,12 +74,33 @@ namespace  SeacrhAlgorthm
 
             } while ((ch == 'y') || (ch == 'Y'));
         }
-        public void LinearSeacrh()
+        public void LinearSearch()
         {
             char ch;
             //search for number of comparison
             int ctr;
+            do
+            {
+                //Accept the number to be searched
+                Console.Write("\nEnter the element you want to search : ");
+                int item = Convert.ToInt32((Console.ReadLine()));
 
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + "found st position " + (i + 1).ToString());
+                        break;
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array");
+                Console.WriteLine("\nNumber of comparison : " + ctr);
+                Console.Write("\nContinue seacrh (y/n):");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
         }
     }
 }
